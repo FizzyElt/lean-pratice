5f45fe26a6f16c69878e708a1651c0b5dd750482
FizzyElt <fizzyelt8786@gmail.com>
2023-07-04 01:25:08 +0800

    format

--- Theory/CalculationalProofs.lean
+++ Theory/CalculationalProofs.lean
@@ -13,11 +13,12 @@ def divides (x y : Nat) : Prop :=
 def divides_trans (h₁ : divides x y) (h₂ : divides y z) : divides x z :=
   let ⟨k₁, d₁⟩ := h₁
   let ⟨k₂, d₂⟩ := h₂
-  ⟨k₁ * k₂,
-  calc
-    k₁ * k₂ * x = k₂ * k₁ * x := congrArg (· * x) (Nat.mul_comm k₁ k₂)
-    _ = k₂ * (k₁ * x) := Nat.mul_assoc k₂ k₁ x
-    _ = k₂ * y := congrArg (k₂ * ·) d₁
-    _ = z := d₂
+  ⟨
+    k₁ * k₂,
+    calc
+      k₁ * k₂ * x = k₂ * k₁ * x := congrArg (· * x) (Nat.mul_comm k₁ k₂)
+      _ = k₂ * (k₁ * x) := Nat.mul_assoc k₂ k₁ x
+      _ = k₂ * y := congrArg (k₂ * ·) d₁
+      _ = z := d₂
   ⟩
   --by rw [Nat.mul_comm k₁ k₂, Nat.mul_assoc, d₁, d₂]
