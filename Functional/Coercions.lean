8254c50f52c0912912a26996c49fb843e498564e
FizzyElt <fizzyelt8786@gmail.com>
2023-07-26 22:27:35 +0800

    Coercion

---
+++ Functional/Coercions.lean
@@ -0,0 +1,30 @@
+def tets : Option Nat := (21 : Nat)
+
+
+structure Monoid where
+  Carrier : Type
+  neutral : Carrier
+  op : Carrier → Carrier → Carrier
+
+def natMulMonoid : Monoid :=
+  { Carrier := Nat, neutral := 1, op := (· * ·) }
+
+def natAddMonoid : Monoid :=
+  { Carrier := Nat, neutral := 0, op := (· + ·) }
+
+def stringMonoid : Monoid :=
+  { Carrier := String, neutral := "", op := String.append }
+
+def listMonoid (α : Type) : Monoid :=
+  { Carrier := List α, neutral := [], op := List.append }
+
+
+def foldMap (M : Monoid) (f : α → M.Carrier) (xs : List α) : M.Carrier :=
+  let rec go (soFar : M.Carrier) : List α → M.Carrier
+    | [] => soFar
+    | y :: ys => go (M.op soFar (f y)) ys
+  go M.neutral xs
+
+
+instance : CoeSort Monoid Type where
+  coe m := m.Carrier
